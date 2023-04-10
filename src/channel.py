8cc4ec0c62bd0b9c0df6695aceb6f444cc44d55d
plantheday <plantheday6@gmail.com>
2023-04-10 20:53:10 +0300

    Homework 2 update

--- src/channel.py
+++ src/channel.py
@@ -15,13 +15,64 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        self.channel = self.__youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.__channel_id = channel_id
+        self.__channel = self.__youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.__title = self.__channel['items'][0]['snippet']['title']
+        self.__video_count = self.__channel['items'][0]['statistics']['videoCount']
+        self.__url = f"https://www.youtube.com/channel/{channel_id}"
+        self.__description = self.__channel['items'][0]['snippet']['description']
+        self.__subscribers_count = self.__channel['items'][0]['statistics']['subscriberCount']
+        self.__view_count = self.__channel['items'][0]['statistics']['viewCount']
+        self.channel = self.__youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.__channel, indent=2, ensure_ascii=False))
 
+    def to_json(self, filename):
+        data = {
+            "channel_id": self.__channel_id,
+            "title": self.__title,
+            "description": self.__description,
+            "url": self.__url,
+            "subscribers_count": self.__subscribers_count,
+            "video_count": self.__video_count,
+            "view_count": self.__view_count
+        }
 
-#vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-#vdud.print_info()
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    @classmethod
+    def get_service(cls):
+        return cls.__youtube
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def video_count(self):
+        return self.__video_count
+
+    @property
+    def url(self):
+        return self.__url
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @property
+    def description(self):
+        return self.__description
+
+    @property
+    def subscribers_count(self):
+        return self.__subscribers_count
+
+    @property
+    def view_count(self):
+        return self.__view_count
+# vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+# vdud.print_info()
